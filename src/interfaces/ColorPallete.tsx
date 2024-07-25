90c88bb50dff9c1f7626abaca4a2070e80b14604
Young Programmer <63667978+imvinhng@users.noreply.github.com>
2024-07-25 11:12:43 +0700

    first commit

---
+++ src/interfaces/ColorPallete.tsx
@@ -0,0 +1,7 @@
+export const white = "#FFFFFF";
+
+export const black = "#000000";
+
+export const darkgray = "#666666";
+
+export const magenta = "#DD1298";
---
+++ src/interfaces/DefaultVariables.tsx
@@ -0,0 +1,13 @@
+export const defaultMargin = 5;
+
+export const defaultPadding = 10;
+
+export const defaultFontSize = 16;
+
+export const fontLarge = defaultFontSize + 5;
+
+export const fontXL = fontLarge + 5;
+
+export const fontXXL = fontXL + 5;
+
+export const fontXXXL = fontXXL + 10;
